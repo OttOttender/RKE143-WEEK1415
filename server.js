f2bb8f29ee4f14e22b050ba5179b7d610e20449b
Ott Ottender <Ott.Ottender@gmail.com>
2024-12-19 23:43:44 +0200

    first commit

--- server.js
+++ server.js
@@ -1,33 +1,22 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const pumpkinRouter = require('./routes/pumpkin.js');
-const rke143Router = require('./routes/rke143');
 
+const express = require('express');
+const recipeRouter = require('./routes/recipes.routes');
+const ingredientRouter = require('./routes/ingredients.routes');
+const fullRecipesRouter = require('./routes/fullRecipes.routes');
+const randomRouter = require('./routes/randomRecipe.routes');
 const app = express();
 
-// Import JSON data
-const pumpkinData = require('./data/pumpkin.json');
-const rke143Data = require('./data/nodejs.json'); // Import nodejs.json (optional)
-
-// Middleware for parsing JSON in request bodies
-app.use(bodyParser.json());
-
-// Root route: Respond with a random pumpkin drink recipe
-app.get('/', (req, res) => {
-    const randomIndex = Math.floor(Math.random() * pumpkinData.Categories['Pumpkin Drinks'].length); 
-    const randomDrinkRecipe = pumpkinData.Categories['Pumpkin Drinks'][randomIndex];
-
-    res.status(200).json({ randomDrinkRecipe });
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
 });
 
-// Mount the pumpkin router at /pumpkin
-app.use('/pumpkin', pumpkinRouter);
+app.use('/ingredients', ingredientRouter);
+app.use('/recipes', recipeRouter);
+app.use('/fullRecipes', fullRecipesRouter);
+app.use('/random', randomRouter);
 
-// Mount the rke143 router at /rke143
-app.use('/rke143', rke143Router);
-
-// Start the server on a dynamic port (for Render or local)
-const PORT = process.env.PORT || 3000; // Use Render's provided port or default to 3000 locally
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}.`);
+app.listen(3000, () => {
+    console.log('Server is running on Port 3000.');
 });
