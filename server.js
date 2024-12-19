7d048f84f094049a28acdd9ac1404cfa6355c48d
Ott Ottender <Ott.Ottender@gmail.com>
2024-12-20 01:02:11 +0200

    first commit

--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-
 const express = require('express');
 const recipeRouter = require('./routes/recipes.routes');
 const ingredientRouter = require('./routes/ingredients.routes');
@@ -6,11 +5,7 @@ const fullRecipesRouter = require('./routes/fullRecipes.routes');
 const randomRouter = require('./routes/randomRecipe.routes');
 const app = express();
 
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-  next();
-});
+app.use(express.json());
 
 app.use('/ingredients', ingredientRouter);
 app.use('/recipes', recipeRouter);
